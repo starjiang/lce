7e4969ab50418e077d105dfbfdacc71b4772bfbe
starjiang <82776315@qq.com>
2014-12-05 16:24:06 +0800

    remove some invalid code of test_echoserver

--- test/Test_EchoServer.cpp
+++ test/Test_EchoServer.cpp
@@ -1,95 +1,41 @@
 #include <iostream>
-#include "Utils.h"
-#include "CEvent.h"
-#include "CCommMgr.h"
-#include "CTask.h"
-#include "CProcessor.h"
-
+#include "lce.h"
 using namespace std;
 using namespace lce;
 
 int iSrv1;
 
 
-class CProCenter : public CTask ,public CProcessor
+class CProCenter : public CProcessor
 {
-
 private:
-
-    CProCenter(){}
-
+    CProCenter() {}
     static CProCenter *m_pInstance;
-
 public:
-	
-
     void onRead(SSession &stSession,const char * pszData, const int iSize)
     {
-
         CCommMgr::getInstance().write(stSession,pszData,iSize,false);
-		
     }
-
-
-    void onWork(int iTaskType,void *pData,int iIndex)
-    {
- 
-    }
-
-
-
-    void onMessage(int dwMsgType,void *pData)
+    void onClose(SSession &stSession)
     {
 
     }
+    void onConnect(SSession &stSession,bool bOk,void *pData)
+    {
 
-
-
-	void onClose(SSession &stSession)
-	{
-
-	}
-
-	void onConnect(SSession &stSession,bool bOk,void *pData)
-	{
-
-	}
-
-
-	void onError(SSession &stSession,const char * szErrMsg,int iError)
-	{
-		cout<<szErrMsg<<endl;
-	}
-
-	void onTimer(int dwTimerId,void *pData)
-	{
-	}
-
-	
-	void onSignal(int iSignal)
-	{
-		switch(iSignal)
-		{
-			case SIGINT:
-			{
-				cout<<"stopping..."<<endl;
-				CCommMgr::getInstance().stop();
-				exit(0);
-			}
-			break;
-		}
-	}
-	
+    }
+    void onError(SSession &stSession,const char * szErrMsg,int iError)
+    {
+        cout<<szErrMsg<<endl;
+    }
     static CProCenter &getInstance()
     {
         if (NULL == m_pInstance)
-       	{
-			m_pInstance = new CProCenter;
-		}
-		return *m_pInstance;
+        {
+            m_pInstance = new CProCenter;
+        }
+        return *m_pInstance;
     }
-
-
 };
 
 
@@ -98,16 +44,13 @@ CProCenter *CProCenter::m_pInstance = NULL;
 int main(int argc,char **argv)
 {
 
-	if(argc < 2)
-	{
-		printf("usage:%s port\n",argv[0]);
-		exit(0);
-	}
+    if(argc < 2)
+    {
+        printf("usage:%s port\n",argv[0]);
+        exit(0);
+    }
 
-    lce::initDaemon(); //后台运行
-
-    CProCenter::getInstance().init(4,50000);
-    CProCenter::getInstance().run();
+    //lce::initDaemon(); //后台运行
 
     if(CCommMgr::getInstance().init(50000) < 0)
     {
@@ -115,19 +58,15 @@ int main(int argc,char **argv)
         return 0;
     }
 
-
     iSrv1 = CCommMgr::getInstance().createSrv(SRV_TCP,"0.0.0.0",atoi(argv[1]));
-	
+
     if(iSrv1 < 0 )
     {
         cout<<CCommMgr::getInstance().getErrMsg()<<endl;
-		return 0;
+        return 0;
     }
 
-	CCommMgr::getInstance().setProcessor(iSrv1,&CProCenter::getInstance(),PKG_RAW);
-
-    CCommMgr::getInstance().addSigHandler(SIGINT,&CProCenter::getInstance());
-
+    CCommMgr::getInstance().setProcessor(iSrv1,&CProCenter::getInstance(),PKG_RAW);
     CCommMgr::getInstance().start();
 
     return 0;
